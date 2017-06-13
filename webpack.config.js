65f00d0ec6323b26e3947a45979a7dc15e4a6d31
Todd Hancock <thancock20@hotmail.com>
2017-06-12 20:40:47 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.styl$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'stylus-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
