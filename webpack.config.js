da76ec96681941ed34ea88c00ba087475bb353a4
Todd Hancock <thancock20@hotmail.com>
2017-06-13 15:38:54 -0500

    Change path to bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
   entry: ['babel-polyfill', './src/index.js'],
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist'
   },
   devServer: {
     watchContentBase: true
